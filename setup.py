7801b773e67fd174b03bc8eede05b4ac6b628b52
Hiroaki <aphiloboe@gmail.com>
2022-07-05 11:40:00 +0900

    release preparation for v1.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def _requires_from_file(filename):
 
 setuptools.setup(
     name="smdconverter",
-    version="1.1.0",
+    version="1.1.1",
     install_requires=_requires_from_file('requirements.txt'),
     author="Hiroaki Takahashi",
     author_email="aphiloboe@gmail.com",
--- smdconverter/constants.py
+++ smdconverter/constants.py
@@ -1,6 +1,6 @@
 from typing_extensions import Literal
 
-VERSION = "1.2.0"
+VERSION = "1.1.1"
 GITHUB_URL = "https://github.com/MiLL4U/smd-converter/releases"
 
 # literals
