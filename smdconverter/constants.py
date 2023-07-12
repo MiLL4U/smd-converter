ba5f031031830ddd4df836f66a2e0f1b01d2dcb5
MiLL4U <aphiloboe@gmail.com>
2023-07-12 14:44:45 +0900

    bump up version to 1.3.2

--- smdconverter/constants.py
+++ smdconverter/constants.py
@@ -3,7 +3,7 @@ from typing import Any, Dict
 
 from typing_extensions import Literal
 
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 GITHUB_URL = "https://github.com/MiLL4U/smd-converter/releases"
 
 # literals
